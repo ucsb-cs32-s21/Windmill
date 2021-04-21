23dcd665519370a643d7117742fcaf6c4e5fd4ee
AvivSamet <aviv@ucsb.edu>
2021-04-21 14:09:06 -0700

    dynamic height

--- windmill.h
+++ windmill.h
@@ -21,13 +21,13 @@ class Windmill{
         Windmill(int width, int height, vec2 center){
             std::vector<vec2> bodyVerts;
             //bottom left
-            bodyVerts.push_back(vec2(center.x() + width/3, center.y() + 80));
+            bodyVerts.push_back(vec2(center.x() + width/3, center.y() + height/4));
             //top left
-            bodyVerts.push_back(vec2(center.x() + width/4, center.y() - 50));
+            bodyVerts.push_back(vec2(center.x() + width/4, center.y() - height/4));
             //top right
-            bodyVerts.push_back(vec2(center.x() - width/4, center.y() + 50));
+            bodyVerts.push_back(vec2(center.x() - width/4, center.y() + height/4));
             //bottom right
-            bodyVerts.push_back(vec2(center.x() - width/3, center.y() + 80));
+            bodyVerts.push_back(vec2(center.x() - width/3, center.y() + height/4));
             
             Polygon body = Polygon(bodyVerts, 1, );
         }
