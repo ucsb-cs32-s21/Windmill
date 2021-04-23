decd2228cd903b222f1ca8f7c9b7fbd2f49c28f4
AvivSamet <aviv@ucsb.edu>
2021-04-23 12:57:23 -0700

    created windmill.cpp

---
+++ windmill.cpp
@@ -0,0 +1,15 @@
+#include "windmill.h"
+
+Windmill::Windmill(int width, int height, vec2 center){
+    std::vector<vec2> bodyVerts;
+    //bottom left
+    bodyVerts.push_back(vec2(center.x() + width/3, center.y() + height/4));
+    //top left
+    bodyVerts.push_back(vec2(center.x() + width/4, center.y() - height/4));
+    //top right
+    bodyVerts.push_back(vec2(center.x() - width/4, center.y() + height/4));
+    //bottom right
+    bodyVerts.push_back(vec2(center.x() - width/3, center.y() + height/4));
+    
+    Polygon body = Polygon(bodyVerts, 1, color(245,184,89));
+}
--- windmill.h
+++ windmill.h
@@ -18,22 +18,7 @@ class Windmill{
         int height;
 
     public:
-        Windmill(int width, int height, vec2 center){
-            std::vector<vec2> bodyVerts;
-            //bottom left
-            bodyVerts.push_back(vec2(center.x() + width/3, center.y() + height/4));
-            //top left
-            bodyVerts.push_back(vec2(center.x() + width/4, center.y() - height/4));
-            //top right
-            bodyVerts.push_back(vec2(center.x() - width/4, center.y() + height/4));
-            //bottom right
-            bodyVerts.push_back(vec2(center.x() - width/3, center.y() + height/4));
-            
-            Polygon body = Polygon(bodyVerts, 1, color(245,184,89));
-        }
-        Windmill(vec2 coords){
-
-        }
+        Windmill(int width, int height, vec2 center);
 
         void setCenter(vec2 cen);
         void setHeight(int height);
